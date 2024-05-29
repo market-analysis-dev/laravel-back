0a37025e51bf7583c0195e614373cf19df181e2c
MaoRusk <manolo_rod.98@hotmail.com>
2024-05-29 11:30:20 -0600

    insert company API

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 
 class CompanyController
 {
-    // * Método para obtener todos los registros
+    // * Método para obtener todos los registros.
     public function index()
     {
-        $companies = Company::all();
+        $companies = Company::where('status', 1)->get();
         return response()->json($companies);
     }
 
-    // * Método para obtener un registro por su ID
+    // * Método para obtener un registro por su ID.
     public function show($id)
     {
         $company = Company::find($id);
@@ -26,7 +26,7 @@ class CompanyController
         return response()->json($company);
     }
 
-    // * Método para editar la información de la empresa por su ID
+    // * Método para editar la información de la empresa por su ID.
     public function update(Request $request, $id)
     {
         // * Validar los datos
@@ -39,10 +39,10 @@ class CompanyController
             'secondary_color' => 'nullable|string|max:7',
         ]);
 
-        // * Buscar la empresa por ID
+        // * Buscar la empresa por ID.
         $company = Company::findOrFail($id);
 
-        // * UPDATE de la empresa
+        // * UPDATE de la empresa.
         $company->update([
             'name_company' => $request->input('name_company'),
             'website' => $request->input('website'),
@@ -53,5 +53,31 @@ class CompanyController
         ]);
 
         return response()->json($company);
+    }
+
+    // * Método para eliminar una epresa.
+    public function delete($id)
+    {
+        // * Buscando la empresa por su ID.
+        $company = Company::findOrFail($id);
+
+        // * Actualizar el estado a 0.
+        $empresa->status = 0;
+        $empresa->save();
+
+        return response()->json(['message' => 'Empresa eliminada correctamente.']);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name_company' => 'required|string|max:255',
+            'website' => 'nullable|url',
+            'logo_url' => 'nullable|string',
+            'address' => 'nullable|string',
+            'primary_color' => 'nullable|string|max:7',
+            'secondary_color' => 'nullable|string|max:7',
+            'status' => 'required|integer|in:0,1',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,9 @@ use App\Http\Controllers\CompanyController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-
 // * COMPANIES
 Route::get('/companies', [CompanyController::class, 'index']);
 Route::get('/companies/{id}', [CompanyController::class, 'show']);
 Route::put('/companies/{id}', [CompanyController::class, 'update']);
+Route::put('/companies/{id}/delete', [CompanyController::class, 'delete']);
+Route::post('/companies', [CompanyController::class, 'store']);
