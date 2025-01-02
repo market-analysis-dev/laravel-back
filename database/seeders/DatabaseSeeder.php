d28450befcf3f56d9229a9e9cce91d71d5964213
anutka03 <akotelnikova@diferenteweb.com>
2025-01-02 19:34:32 +0200

    CRUD Tenant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             RegionSeeder::class,
             MarketSeeder::class,
             DeveloperSeeder::class,
+            TenantSeeder::class,
         ]);
     }
 }
---
+++ routes/_tenants.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\TenantController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix' => 'tenants',
+    'as' => 'api.tenants',
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::get('/', [TenantController::class, 'index'])->name('index');
+    Route::post('/', [TenantController::class, 'store'])->name('store');
+    Route::get('/{tenant}', [TenantController::class, 'show'])->name('show');
+    Route::put('/{tenant}', [TenantController::class, 'update'])->name('update');
+    Route::delete('/{tenant}', [TenantController::class, 'destroy'])->name('destroy');
+});
