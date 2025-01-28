af18729bcc4b3eaf057191840da3220c929f0252
anutka03 <akotelnikova@diferenteweb.com>
2025-01-28 17:34:10 +0100

    bugfix - actualizar developer error unique

--- app/Http/Requests/UpdateDeveloperRequest.php
+++ app/Http/Requests/UpdateDeveloperRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateDeveloperRequest extends FormRequest
 {
@@ -22,7 +23,12 @@ class UpdateDeveloperRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:100|unique:cat_developers,name',
+            'name' => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('cat_developers', 'name')->ignore($this->developer),
+            ],
             'is_developer' => 'required|boolean',
             'is_builder' => 'required|boolean',
             'is_owner' => 'required|boolean',
