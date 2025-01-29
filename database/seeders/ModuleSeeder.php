2dde4b117868c537716a62349d493e461df776a3
anutka03 <akotelnikova@diferenteweb.com>
2025-01-29 16:55:13 +0100

    API Modules

---
+++ database/seeders/ModuleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ModuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            ['name' => 'Availability'],
+            ['name' => 'Absorption'],
+            ['name' => 'Bi Charts'],
+            ['name' => 'Overview'],
+            ['name' => 'Land Available'],
+            ['name' => 'Land Absorption'],
+            ['name' => 'CAM'],
+            ['name' => 'Negative Absorption'],
+            ['name' => 'Analytics Availability'],
+            ['name' => 'Analytics Absorption'],
+            ['name' => 'Market Report'],
+            ['name' => 'Market Size'],
+            ['name' => 'Industrial Park'],
+            ['name' => 'Fibers'],
+        ];
+    }
+}
