0d9ea49b2e2c7c431c2f282cda266177f6adf602
MaoRusk <manolo_rod.98@hotmail.com>
2025-02-20 12:45:23 -0600

    migrations fixed

--- database/migrations/2025_02_18_193226_create_permissions_policies_tables.php
+++ database/migrations/2025_02_18_193226_create_permissions_policies_tables.php
@@ -5,43 +5,44 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+    
     public function up()
     {
         Schema::create('permissions_policies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('module_id')->constrained('modules')->onDelete('cascade');
-            $table->foreignId('market_id')->constrained('markets')->onDelete('cascade');
-            $table->foreignId('sub_market_id')->constrained('sub_markets')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('module_id')->constrained('modules');
+            $table->foreignId('market_id')->constrained('markets');
+            $table->foreignId('sub_market_id')->constrained('sub_markets');
             $table->integer('year')->nullable();
             $table->string('quarter')->nullable();
-            $table->enum('status', ['Activo', 'Inactivo'])->default('Activo');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
             $table->softDeletes();
-            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
-            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
         });
 
         Schema::create('permissions_unique', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('permission_id')->constrained('permissions_policies')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('permission_id')->constrained('permissions_policies');
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
             $table->softDeletes();
-            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
-            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
         });
 
         Schema::create('permissions_submodules', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('sub_module_id')->constrained('sub_modules')->onDelete('cascade');
-            $table->enum('status', ['Activo', 'Inactivo'])->default('Activo');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('sub_module_id')->constrained('sub_modules');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
             $table->softDeletes();
-            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
-            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
         });
     }
 
