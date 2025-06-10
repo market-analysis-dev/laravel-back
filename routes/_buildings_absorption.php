4fa140da85f81d3fb5485d65903d136236978b8d
Michael Ordaz Mrtz <tu_nuevo_correo@example.com>
2025-06-10 17:20:30 -0600

    correcion de nombre de parametro en abs

--- routes/_buildings_absorption.php
+++ routes/_buildings_absorption.php
@@ -10,13 +10,13 @@ Route::group([
 ], function () {
     Route::get('/', [BuildingsAbsorptionController::class, 'index'])->name('index');
     Route::post('/', [BuildingsAbsorptionController::class, 'store'])->name('store');
-    Route::get('/{buildingAvailable}', [BuildingsAbsorptionController::class, 'show'])->name('show');
-    Route::put('/{buildingAvailable}', [BuildingsAbsorptionController::class, 'update'])->name('update');
-    Route::delete('/{buildingAvailable}', [BuildingsAbsorptionController::class, 'destroy'])->name('destroy');
+    Route::get('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'show'])->name('show');
+    Route::put('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'update'])->name('update');
+    Route::delete('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'destroy'])->name('destroy');
 
-    Route::put('/{buildingAvailable}/to-available', [BuildingsAbsorptionController::class, 'toAvailable'])->name('to-available');
-    Route::post('/{buildingAvailable}/draft', [BuildingsAbsorptionController::class, 'draft'])->name('draft');
-    Route::get('/{buildingAvailable}/draft', [BuildingsAbsorptionController::class, 'getDraft'])->name('getDraft');
-    Route::put('/{buildingAvailable}/draft', [BuildingsAbsorptionController::class, 'updateDraft'])->name('updateDraft');
-    Route::delete('/{buildingAvailable}/draft', [BuildingsAbsorptionController::class, 'deleteDraft'])->name('deleteDraft');
+    Route::put('/{buildingAbsorption}/to-available', [BuildingsAbsorptionController::class, 'toAvailable'])->name('to-available');
+    Route::post('/{buildingAbsorption}/draft', [BuildingsAbsorptionController::class, 'draft'])->name('draft');
+    Route::get('/{buildingAbsorption}/draft', [BuildingsAbsorptionController::class, 'getDraft'])->name('getDraft');
+    Route::put('/{buildingAbsorption}/draft', [BuildingsAbsorptionController::class, 'updateDraft'])->name('updateDraft');
+    Route::delete('/{buildingAbsorption}/draft', [BuildingsAbsorptionController::class, 'deleteDraft'])->name('deleteDraft');
 });
