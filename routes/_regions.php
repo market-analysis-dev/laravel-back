d9f55bedc7dd20dc5463cf01617c88d51eef078a
MaoRusk <manolo_rod.98@hotmail.com>
2025-06-16 13:18:08 -0600

    APIs CRUD Region

--- routes/_regions.php
+++ routes/_regions.php
@@ -7,8 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'regions',
     'as' => 'api.regions.',
-    'middleware' => 'auth:sanctum',
+    // 'middleware' => 'auth:sanctum',
 ], function () {
     Route::get('/', [RegionController::class, 'index'])->name('index');
+    Route::post('/', [RegionController::class, 'store'])->name('store');
+    Route::get('/{region}', [RegionController::class, 'show'])->name('show');
+    Route::put('/{region}', [RegionController::class, 'update'])->name('update');
+    Route::delete('/{region}', [RegionController::class, 'destroy'])->name('destroy');
 });
 
