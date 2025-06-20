4c85bb949423b5e253cb2996842423ea3bf277b6
anutka03 <akotelnikova@diferenteweb.com>
2025-06-20 18:50:52 +0200

    Avl Export

--- routes/_buildings_absorption.php
+++ routes/_buildings_absorption.php
@@ -10,6 +10,7 @@ Route::group([
 ], function () {
     Route::get('/', [BuildingsAbsorptionController::class, 'index'])->name('index');
     Route::post('/', [BuildingsAbsorptionController::class, 'store'])->name('store');
+    Route::get('/export', [BuildingsAbsorptionController::class, 'exportAbsorption'])->name('exportAbsorption');
     Route::get('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'show'])->name('show');
     Route::post('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'update'])->name('update');
     Route::delete('/{buildingAbsorption}', [BuildingsAbsorptionController::class, 'destroy'])->name('destroy');
